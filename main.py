166a3d40c05d0ce3e747c17b7e3a10a1f49ac7bc
Harman gill <95903218+Harmangillcs@users.noreply.github.com>
2025-09-21 19:18:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,26 +8,32 @@ from huggingface_hub import hf_hub_download
 import os
 from dotenv import load_dotenv
 
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  
+
+
+st.set_option('browser.gatherUsageStats', False)
+
+
 load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")  # Hugging Face token
+
 
 BASE_DIR = Path(__file__).resolve().parent
 CLASS_INDICES_PATH = BASE_DIR / "app" / "class_indices.json"
 
-# Hugging Face model details
 HF_MODEL_REPO = "harmancs/Plants_Disease_Prediction"
 HF_MODEL_FILENAME = "plant_disease_prediction_model.h5"
 
-
-HF_TOKEN = os.getenv("HF_TOKEN")  
 MODEL_PATH = hf_hub_download(
-    repo_id="harmancs/Plants_Disease_Prediction",
-    filename="plant_disease_prediction_model.h5",
+    repo_id=HF_MODEL_REPO,
+    filename=HF_MODEL_FILENAME,
     use_auth_token=HF_TOKEN
 )
 model = tf.keras.models.load_model(MODEL_PATH)
 st.success("Model loaded successfully!")
 
-# Load class indices
+
 with open(CLASS_INDICES_PATH) as f:
     class_indices = json.load(f)
 
@@ -35,11 +41,11 @@ with open(CLASS_INDICES_PATH) as f:
 def load_and_preprocess_image(image, target_size=(224, 224)):
     if not isinstance(image, Image.Image):
         image = Image.open(image)
-        img = image.resize(target_size)
-        img_array = np.array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array = img_array.astype('float32') / 255.0
-        return img_array
+    img = image.resize(target_size)
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array.astype('float32') / 255.0
+    return img_array
 
 
 def predict_image_class(model, image, class_indices):
